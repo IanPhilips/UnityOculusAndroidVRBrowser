1865fcb892caaf57b995f5399c8651df7c43190e
Ian Philips <iansphilips@gmail.com>
2021-01-03 21:44:15 -0700

    update gecko library to 84.0.2x

--- GeckoViewPlugin/app/.externalNativeBuild/cmake/debug/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ GeckoViewPlugin/app/.externalNativeBuild/cmake/debug/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Darwin-19.5.0")
+set(CMAKE_HOST_SYSTEM "Darwin-19.6.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "19.5.0")
+set(CMAKE_HOST_SYSTEM_VERSION "19.6.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/Users/ianphilips/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
